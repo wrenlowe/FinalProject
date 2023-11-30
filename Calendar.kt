1a7dc9b9aee6d2202e698475dc2e4be4132cd84d
Wren Lowe <115882181+wrenlowe@users.noreply.github.com>
2023-11-30 09:56:27 -0500

    Update Calendar.kt

--- Calendar.kt
+++ Calendar.kt
@@ -1,20 +1,38 @@
 package com.example.finalproject
 
 class Calendar {
-    private var numItems : Int = 0
-    private lateinit var list : HashMap<String, ArrayList<String>>
-
-    fun getNumItems() : Int {
-        return numItems
-    }
+      private lateinit var list : HashMap<String, ArrayList<String>>
+    
     fun getList() : HashMap<String, ArrayList<String>>{
         return list
     }
     fun getListForDay(day : String) : ArrayList<String>?{
         return list.get(day)
     }
-    fun setNumItems(numItems : Int){
-        this.numItems = numItems
+    fun setList(list : HashMap<String, ArrayList<String>>){
+        this.list = list
+    }
+    fun setListForDay(day: String, l: ArrayList<String>){
+        list.set(day, l)
+    }
+    fun addItem(day: String, l: String){
+        var oldList : ArrayList<String>? = list.get(day)
+        if (oldList == null){
+            var newList : ArrayList<String> = arrayListOf(l)
+            list.set(day, newList)
+        } else {
+            oldList.add(l)
+            list.set(day, oldList)
+        }
+    }
+    fun removeDay(day: String){
+        list.remove(day)
+    }
+    fun removeItem(day : String, l: String){
+        var oldList : ArrayList<String>? = list.get(day)
+        if (oldList != null){
+            oldList.remove(l)
+        }
     }
 
 
