a53ed3c1f59b7ae78fa36ebd6e2de790519f73a5
Wren Lowe <115882181+wrenlowe@users.noreply.github.com>
2023-11-30 10:41:31 -0500

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.finalproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ListView
@@ -13,15 +15,13 @@ class MainActivity : AppCompatActivity() {
     private lateinit var listView : ListView
 
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         listView = findViewById<ListView>( R.id.todo )
         calendar = Calendar()
-        // just to see if the listView was working correctly
+
         calendar.addItem("11302023", "Finish Project")
         displayList("11302023")
 
@@ -33,8 +33,9 @@ class MainActivity : AppCompatActivity() {
                 var adapter : ArrayAdapter<String> =
                     ArrayAdapter<String>( this, android.R.layout.simple_list_item_multiple_choice , calendar.getListForDay(day)!! )
                 listView.setAdapter( adapter )
+                var lih : ListItemHandler = ListItemHandler( )
+                listView.setOnItemClickListener( lih )
             }
-
         }
     }
 
@@ -62,11 +63,29 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
-    fun switchScreen (v : View){
+    // should probably show a screen where it tells them to enter the date in a specific way
+    // and the activity they want to enter
+    fun addTask(v : View){
+
+    }
+
+    fun monthlyList (v : View){
         // update
         var intent:Intent = Intent(this, DataActivity::class.java)
         startActivity(intent)
 
+    }
+    fun weeklyList (v : View){
+        // update
+        // need to change to a different activity class
+        var intent:Intent = Intent(this, DataActivity::class.java)
+        startActivity(intent)
+    }
+    // need to deal with event handling when the user checks that they have finished a task
+    inner class ListItemHandler : AdapterView.OnItemClickListener {
+        override fun onItemClick(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            Log.w("MainActivity", "check if this is the right listener ")
+        }
     }
     companion object{
         lateinit var calendar: Calendar
