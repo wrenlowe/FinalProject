5ec8bc768a05e8b91300d72ced96cb6382c62720
Wren Lowe <115882181+wrenlowe@users.noreply.github.com>
2023-11-30 12:56:17 -0500

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,19 +3,17 @@ package com.example.finalproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.EditText
 import android.widget.ListView
-import android.widget.RadioButton
-import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var listView : ListView
+    private val formatter = DateTimeFormatter.ofPattern("MMddyyyy")
+    private var current : String = LocalDateTime.now().format(formatter)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,8 +24,6 @@ class MainActivity : AppCompatActivity() {
         calendar = Calendar()
 
         calendar.addItem("11302023", "Finish Project")
-        val formatter = DateTimeFormatter.ofPattern("MMddyyyy")
-        val current : String = LocalDateTime.now().format(formatter)
         displayList(current)
 
 
@@ -50,6 +46,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        displayList(current)
     }
 
     override fun onPause() {
@@ -71,26 +68,24 @@ class MainActivity : AppCompatActivity() {
     // should probably show a screen where it tells them to enter the date in a specific way
     // and the activity they want to enter
     fun addTask(v : View){
-
+        var intent:Intent = Intent(this, TaskActivity::class.java)
+        startActivity(intent)
     }
-
-    fun monthlyList (v : View){
+    fun calendarSwitch (v : View){
         // update
-        var intent:Intent = Intent(this, DataActivity::class.java)
+        var intent:Intent = Intent(this, CalendarActivity::class.java)
         startActivity(intent)
 
     }
-    fun weeklyList (v : View){
+    fun weekSwitch (v : View){
         // update
         // need to change to a different activity class
-        var intent:Intent = Intent(this, DataActivity::class.java)
+        var intent:Intent = Intent(this, WeekActivity::class.java)
         startActivity(intent)
     }
     // need to deal with event handling when the user checks that they have finished a task
     inner class ListItemHandler : AdapterView.OnItemClickListener {
         override fun onItemClick(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            val formatter = DateTimeFormatter.ofPattern("MMddyyyy")
-            val current : String = LocalDateTime.now().format(formatter)
             var oldList : ArrayList<String> = calendar.getListForDay(current)!!
             oldList.removeAt(position)
             displayList(current)
