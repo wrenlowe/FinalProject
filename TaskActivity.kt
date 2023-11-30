96a8aa5ab2f110b351ca3d50af858bac745527cb
Wren Lowe <115882181+wrenlowe@users.noreply.github.com>
2023-11-30 12:07:12 -0500

    Add files via upload

---
+++ TaskActivity.kt
@@ -0,0 +1,15 @@
+package com.example.finalproject
+
+import android.os.Bundle
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class TaskActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+
+    }
+}
