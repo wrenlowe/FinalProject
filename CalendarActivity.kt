80e736f400d4346af9d899ddfdc743794388786a
Wren Lowe <115882181+wrenlowe@users.noreply.github.com>
2023-11-30 20:57:32 -0500

    Update CalendarActivity.kt

--- CalendarActivity.kt
+++ CalendarActivity.kt
@@ -7,12 +7,17 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.CalendarView
-import android.widget.EditText
 import android.widget.ListView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+
 
 class CalendarActivity : AppCompatActivity() {
     private lateinit var listView : ListView
@@ -20,10 +25,17 @@ class CalendarActivity : AppCompatActivity() {
     private val formatter = DateTimeFormatter.ofPattern("MMddyyyy")
     private lateinit var clh : CalendarListHandler
     private  var current : String = LocalDateTime.now().format(formatter)
+    private var ad : InterstitialAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+
+        var adUnitId : String = // "ca-app-pub-3940256099942544/6300978111"
+            "ca-app-pub-3940256099942544/1033173712"
+        var adRequest : AdRequest = (AdRequest.Builder( )).build( )
+        var adLoad : AdLoad = AdLoad( )
+        InterstitialAd.load( this, adUnitId, adRequest, adLoad )
 
         listView = findViewById( R.id.list )
         calendarView = findViewById(R.id.simpleCalendarView)
@@ -43,6 +55,52 @@ class CalendarActivity : AppCompatActivity() {
             }
         }
 
+    }
+    inner class AdLoad : InterstitialAdLoadCallback() {
+        override fun onAdFailedToLoad(p0: LoadAdError) {
+            super.onAdFailedToLoad(p0)
+            Log.w( "MainActivity", "ad failed to load" )
+        }
+
+        override fun onAdLoaded(p0: InterstitialAd) {
+            super.onAdLoaded(p0)
+            Log.w( "MainActivity", "ad loaded" )
+            // show the ad
+            ad = p0
+            ad!!.show( this@CalendarActivity )
+
+            // handle user interaction with the ad
+            var manageAd : ManageAdShowing = ManageAdShowing()
+            ad!!.fullScreenContentCallback = manageAd
+        }
+    }
+
+    inner class ManageAdShowing : FullScreenContentCallback( ) {
+        override fun onAdDismissedFullScreenContent() {
+            super.onAdDismissedFullScreenContent()
+            Log.w( "MainActivity", "user closed the ad" )
+            // some code here to continue the app
+        }
+
+        override fun onAdClicked() {
+            super.onAdClicked()
+            Log.w( "MainActivity", "User clicked on the ad" )
+        }
+
+        override fun onAdImpression() {
+            super.onAdImpression()
+            Log.w( "MainActivity", "user has seen the ad" )
+        }
+
+        override fun onAdShowedFullScreenContent() {
+            super.onAdShowedFullScreenContent()
+            Log.w( "MainActivity", "ad has been shown" )
+        }
+
+        override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+            super.onAdFailedToShowFullScreenContent(p0)
+            Log.w( "MainActivity", "ad failed to show" )
+        }
     }
     fun addTask(v : View){
         var intent: Intent = Intent(this, TaskActivity::class.java)
