d336fc976cf6fcfeed8c576e0d2edb1f3d589c90
Wren Lowe <115882181+wrenlowe@users.noreply.github.com>
2023-11-30 20:56:53 -0500

    Update WeekActivity.kt

--- WeekActivity.kt
+++ WeekActivity.kt
@@ -1,18 +1,78 @@
 package com.example.finalproject
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdError
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.FullScreenContentCallback
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 
 class WeekActivity : AppCompatActivity() {
 
+    private var ad : InterstitialAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_week)
 
+        var adUnitId : String = // "ca-app-pub-3940256099942544/6300978111"
+            "ca-app-pub-3940256099942544/1033173712"
+        var adRequest : AdRequest = (AdRequest.Builder( )).build( )
+        var adLoad : AdLoad = AdLoad( )
+        InterstitialAd.load( this, adUnitId, adRequest, adLoad )
+
+    }
+    inner class AdLoad : InterstitialAdLoadCallback() {
+        override fun onAdFailedToLoad(p0: LoadAdError) {
+            super.onAdFailedToLoad(p0)
+            Log.w( "MainActivity", "ad failed to load" )
+        }
+
+        override fun onAdLoaded(p0: InterstitialAd) {
+            super.onAdLoaded(p0)
+            Log.w( "MainActivity", "ad loaded" )
+            // show the ad
+            ad = p0
+            ad!!.show( this@WeekActivity )
+
+            // handle user interaction with the ad
+            var manageAd : ManageAdShowing = ManageAdShowing()
+            ad!!.fullScreenContentCallback = manageAd
+        }
+    }
+
+    inner class ManageAdShowing : FullScreenContentCallback( ) {
+        override fun onAdDismissedFullScreenContent() {
+            super.onAdDismissedFullScreenContent()
+            Log.w( "MainActivity", "user closed the ad" )
+            // some code here to continue the app
+        }
+
+        override fun onAdClicked() {
+            super.onAdClicked()
+            Log.w( "MainActivity", "User clicked on the ad" )
+        }
+
+        override fun onAdImpression() {
+            super.onAdImpression()
+            Log.w( "MainActivity", "user has seen the ad" )
+        }
+
+        override fun onAdShowedFullScreenContent() {
+            super.onAdShowedFullScreenContent()
+            Log.w( "MainActivity", "ad has been shown" )
+        }
+
+        override fun onAdFailedToShowFullScreenContent(p0: AdError) {
+            super.onAdFailedToShowFullScreenContent(p0)
+            Log.w( "MainActivity", "ad failed to show" )
+        }
     }
 
     override fun onStart() {
